cc877711998a4bea49efffefacba2b2846aef16c
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-05 01:19:33 +0800

    mi8937: Workaround GPIO flashlight issue

--- recovery/root/system/bin/set-device-props.sh
+++ recovery/root/system/bin/set-device-props.sh
@@ -16,6 +16,11 @@ set_device_model() {
 	done
 }
 
+workaround_gpio_flashlight() {
+	echo 1 > /sys/class/leds/flashlight/max_brightness;
+	echo 0 > /sys/class/leds/flashlight/brightness;
+}
+
 case "$(cat /sys/firmware/devicetree/base/model)" in
 	"Qualcomm Technologies, Inc. MSM8917-PMI8937 QRD SKU5")
 		if [ -e /sys/class/leds/infrared/transmit ]; then
@@ -25,18 +30,22 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 			set_device_codename "riva"
 			set_device_model "Redmi 5A (Nougat bootloader)"
 		fi
+		workaround_gpio_flashlight
 		;;
 	"Qualcomm Technologies, Inc. MSM8917 QRD SKU5")
 		set_device_codename "riva"
 		set_device_model "Redmi 5A (Oreo bootloader)"
+		workaround_gpio_flashlight
 		;;
 	"Qualcomm Technologies, Inc. MSM8917-PMI8937 MTP")
 		set_device_codename "ugglite"
 		set_device_model "Redmi Note 5A / Y1 Lite"
+		workaround_gpio_flashlight
 		;;
 	"Qualcomm Technologies, Inc. MSM8940-PMI8937 MTP")
 		set_device_codename "ugg"
 		set_device_model "Redmi Note 5A / Y1 Prime"
+		workaround_gpio_flashlight
 		;;
 	"Qualcomm Technologies, Inc. MSM8937-PMI8950 QRD SKU1")
 		set_device_codename "land"
