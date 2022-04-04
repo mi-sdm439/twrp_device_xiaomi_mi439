77cbb56c313047ee4ec1b715f5a1fbb21c967984
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-04 14:52:00 +0800

    mi8937: Update makefile inherits

--- omni_mi8937.mk
+++ omni_mi8937.mk
@@ -17,10 +17,10 @@
 # Release name
 PRODUCT_RELEASE_NAME := mi8937
 
-$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product-if-exists, vendor/omni/config/common.mk)
 
 # Inherit from mi8937 device
 $(call inherit-product, device/xiaomi/mi8937/device.mk)
