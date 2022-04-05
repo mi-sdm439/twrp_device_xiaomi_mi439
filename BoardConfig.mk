afec7ea34c1fa16cdfb07f4cc80e0cd99e818132
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-05 21:56:10 +0800

    mi439: Define firmware search path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_CMDLINE += console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0x78B0000 loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.usbconfigfs=true androidboot.selinux=permissive firmware_class.path=/vendor/firmware
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DTBO := true
