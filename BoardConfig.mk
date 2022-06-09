901bf90d745c1043fe6d5be22ea8fa7a71ef7d8f
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-09 19:05:31 +0800

    mi439: Build vendor.display.config libraries from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,10 +119,15 @@ RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/tombstoned
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libandroidicu \
-    libxml2
+    libxml2 \
+    vendor.display.config@1.0 \
+    vendor.display.config@2.0
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
+
 
 include $(DEVICE_PATH)/BoardConfigOFOX.mk
 include $(DEVICE_PATH)/BoardConfigPBRP.mk
