4e0d7107f61f79092e7de34a08d08f4866c8c661
me-cafebabe <me_cafebabe@hotmail.com>
2022-06-24 03:21:47 +0800

    mi439: Protect block devices which shouldn't be changed
    Recently, Some peoples on Telegram are spreading malicious flashable ZIPs that attempts to brick others' devices.
    
    This commit is intended to protect the users against such attacks.

---
+++ recovery/root/system/bin/protect-partitions.sh
@@ -0,0 +1,16 @@
+#!/system/bin/sh -e
+
+# Manually Set Variables
+BOOTDEVICE="$(getprop ro.boot.bootdevice)"
+UNPROTECTED_PARTITIONS_REGEX="^boot$|^dtbo$|^recovery$|^vbmeta$|^cache$|^userdata$|^system$|^vendor$|^cust$|^misc$|^persist$|^splash$|^fs.$|^modemst.$"
+
+# Generated variables
+PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval grep -vE \"${UNPROTECTED_PARTITIONS_REGEX}\")"
+
+# Make protected partitions read-only
+log "Protected partitions: " $PROTECTED_PARTITIONS
+for p in $PROTECTED_PARTITIONS; do
+    blockdev --setro /dev/block/platform/soc/${BOOTDEVICE}/by-name/${p} || true
+done
+
+exit 0
