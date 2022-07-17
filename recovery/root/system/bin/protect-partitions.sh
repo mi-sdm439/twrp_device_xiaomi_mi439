78a8009646b1aa46de22ddc98bf05bc3738acd37
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-17 15:51:08 +0800

    mi439: Use logdump partition as metadata

--- recovery/root/system/bin/protect-partitions.sh
+++ recovery/root/system/bin/protect-partitions.sh
@@ -2,7 +2,7 @@
 
 # Manually Set Variables
 BOOTDEVICE="$(getprop ro.boot.bootdevice)"
-UNPROTECTED_PARTITIONS_REGEX="^boot$|^dtbo$|^recovery$|^vbmeta$|^cache$|^userdata$|^system$|^vendor$|^cust$|^misc$|^persist$|^splash$|^fs.$|^modemst.$"
+UNPROTECTED_PARTITIONS_REGEX="^boot$|^dtbo$|^recovery$|^vbmeta$|^cache$|^userdata$|^system$|^vendor$|^cust$|^logdump$|^misc$|^persist$|^splash$|^fs.$|^modemst.$"
 
 # Generated variables
 PROTECTED_PARTITIONS="$(ls /dev/block/platform/soc/${BOOTDEVICE}/by-name/|eval grep -vE \"${UNPROTECTED_PARTITIONS_REGEX}\")"
